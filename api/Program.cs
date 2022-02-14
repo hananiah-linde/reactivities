c3f859721122b34e9c9ff540d99c02e77b715233
hananiah-linde <57416903+hananiah-linde@users.noreply.github.com>
2022-02-14 14:48:35 -0600

    End of section 3

--- api/Program.cs
+++ api/Program.cs
@@ -14,6 +14,13 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
 
 var app = builder.Build();
 
@@ -39,6 +46,10 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
